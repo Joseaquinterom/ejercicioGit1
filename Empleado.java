833cebc071ddaaa8522af0533472db84c206c92c
José Antonio Quintero Maya <joseaquinterom@gmail.com>
2022-12-05 10:42:46 +0100

    Programa que crea y muestra un objeto empleado

---
+++ Empleado.java
@@ -0,0 +1,20 @@
+/**
+ * @Author: José Antonio Quintero Maya
+ */
+import java.util.Date;
+
+class Empleado {
+    String nombre;
+    Date birthDate;
+
+    Empleado(String name, Date birthDate) {
+        this.nombre = name;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado: " + nombre + " (nacido en 19" + birthDate.getYear() + ")";
+    }
+
+}
